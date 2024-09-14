7d3bc7f5f3c7547ae69970dd11fe683dafbc2558
Jie clark Terec <jieclarkm@gmail.com>
2024-09-15 07:14:41 +0800

    remove App.css

--- src/PlayMakersHome/App.jsx
+++ src/PlayMakersHome/App.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import './App.css';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate for routing
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
   const [popupVisible, setPopupVisible] = useState(false);
@@ -15,28 +14,37 @@ const App = () => {
   };
 
   return (
-    <div id="blur" className={popupVisible ? 'active' : ''}>
+    <div id="blur" className={popupVisible ? "active" : ""}>
       <header className="flex items-center justify-between p-4 shadow-md">
-    <h1 className="text-xl font-bold text-[#FFFFFF]">Landing</h1>
-    <div className="flex items-center space-x-4">
-      <button
-        onClick={handleLoginClick}
-        className="px-4 py-2 bg-[#FFFFFF] text-black rounded hover:bg-[#9e9c9d]"
-      >
-        Login
-      </button>
-    </div>
-  </header>
+        <h1 className="text-xl font-bold text-[#FFFFFF]">Landing</h1>
+        <div className="flex items-center space-x-4">
+          <button
+            onClick={handleLoginClick}
+            className="px-4 py-2 bg-[#FFFFFF] text-black rounded hover:bg-[#9e9c9d]"
+          >
+            Login
+          </button>
+        </div>
+      </header>
       <main>
         <div className="main-content">
-          <img src="playmakerslogo.png" alt="Playmakers Logo" className="logo" />
+          <img
+            src="playmakerslogo.png"
+            alt="Playmakers Logo"
+            className="logo"
+          />
           <div className="main-text-container">
-            <h1 className="main-text">Exploring Music<br />Within You</h1>
-            <a href="#" className="sub-text">About us -&gt;</a>
+            <h1 className="main-text">
+              Exploring Music
+              <br />
+              Within You
+            </h1>
+            <a href="#" className="sub-text">
+              About us -&gt;
+            </a>
           </div>
         </div>
       </main>
-      
     </div>
   );
 };
